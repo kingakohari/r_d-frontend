60442168c788141b85a08ddc60277efaac246b16
Kinga Kohari <kinguca@yahoo.com>
2024-01-13 16:55:26 +0000

    Added reading time calculation

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function loadEvent() {
 
+    function readingTime() {
+        const text = document.querySelector('article').innerText;
+        const wpm = 250;             // Average reading speed (words per minute)
+        const words = text.trim().split(/\s+/).length;  // Calculate total number of words (length) by splitting at each whitespace
+        const time = Math.ceil(words / wpm);  // Calculate the read time rounded up to the nearest whole number
+        document.querySelector('h1').insertAdjacentHTML('afterend', `
+        <p><span id="time"></span>(Estimated reading time: ${time} minutes)</p>
+        `)
+      }
+      readingTime();
+
     const searchBox = document.getElementById('searchBox');
     const article = document.querySelector('article');
     const originalContent = article.innerHTML; 
@@ -7,7 +18,7 @@ function loadEvent() {
     searchBox.addEventListener('input', function() {
         let searchText = this.value;
         if (searchText.length < 2) {
-            article.innerHTML = originalContent; // No search for only a single character typed in
+            article.innerHTML = originalContent;  // No search for only a single character typed in
             return;
         }
         highlightText(searchText);
@@ -16,10 +27,10 @@ function loadEvent() {
 
     function highlightText(word) {
         if (word.trim() === '') {
-            article.innerHTML = originalContent; // Reset to original before new search (remove any highlights)
+            article.innerHTML = originalContent;  // Reset to original before new search (remove any highlights)
         } else {
-            const regex = new RegExp(`\\b(${word})\\b`, 'gi'); // Only full words are matched + case insensitive
-            article.innerHTML = originalContent.replace(regex, '<span class="highlight">$1</span>'); // Returns any text matching "word" in a span
+            const regex = new RegExp(`\\b(${word})\\b`, 'gi');  // Only full words are matched + case insensitive
+            article.innerHTML = originalContent.replace(regex, '<span class="highlight">$1</span>');  // Returns any text matching "word" in a span
         }
     }
 
