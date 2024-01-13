f595194b5ad60cd1800a679a23f53bef56fa95a7
Kinga Kohari <kinguca@yahoo.com>
2024-01-13 21:10:39 +0000

    API call to retrieve author OK

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
-function loadEvent() {
+async function loadEvent() {
+  const article = document.querySelector("article");
 
-    function readingTime() {
-        const text = document.querySelector('article').innerText;
-        const wpm = 250;             // Average reading speed (words per minute)
-        const words = text.trim().split(/\s+/).length;  // Calculate total number of words (length) by splitting at each whitespace
-        const time = Math.ceil(words / wpm);  // Calculate the read time rounded up to the nearest whole number
-        document.querySelector('h1').insertAdjacentHTML('afterend', `
+// Hf 6-1
+
+  function readingTime() {
+    const text = article.innerText;
+    const wpm = 250; // Average reading speed (words per minute)
+    const words = text.trim().split(/\s+/).length; // Calculate total number of words (length) by splitting at each whitespace
+    const time = Math.ceil(words / wpm); // Calculate the read time rounded up to the nearest whole number
+    document.querySelector("h1").insertAdjacentHTML(
+      "afterend",
+      `
         <p><span id="time"></span>(Estimated reading time: ${time} minutes)</p>
-        `)
-      }
-      readingTime();
-
-    const searchBox = document.getElementById('searchBox');
-    const article = document.querySelector('article');
-    const originalContent = article.innerHTML; 
-
-    searchBox.addEventListener('input', function() {
-        let searchText = this.value;
-        if (searchText.length < 2) {
-            article.innerHTML = originalContent;  // No search for only a single character typed in
-            return;
-        }
-        highlightText(searchText);
-        }
+        `
     );
+  }
+  readingTime();
 
-    function highlightText(word) {
-        if (word.trim() === '') {
-            article.innerHTML = originalContent;  // Reset to original before new search (remove any highlights)
-        } else {
-            const regex = new RegExp(`\\b(${word})\\b`, 'gi');  // Only full words are matched + case insensitive
-            article.innerHTML = originalContent.replace(regex, '<span class="highlight">$1</span>');  // Returns any text matching "word" in a span
-        }
+// Hf 6-2
+
+  const searchBox = document.getElementById("searchBox");
+  const originalContent = article.innerHTML;
+
+  searchBox.addEventListener("input", function () {
+    let searchText = this.value;
+    if (searchText.length < 2) {
+      article.innerHTML = originalContent; // No search for only 1 character typed in
+      return;
     }
+    highlightText(searchText);
+  });
 
-    /* fetch("./public/data.json")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
+  function highlightText(word) {
+    if (word.trim() === "") {
+      article.innerHTML = originalContent; // Reset to original before new search (remove any highlights)
+    } else {
+      const regex = new RegExp(`\\b(${word})\\b`, "gi"); // Only full words are matched + case insensitive
+      article.innerHTML = originalContent.replace(
+        regex,
+        '<span class="highlight">$1</span>' // Returns any text matching "word" in a span
+      ); 
+    }
+  }
 
-      document.getElementById("root").insertAdjacentHTML('beforeend', `
-        <header>
-            <p id="title">Best Beers</p>
-            <button class="material-icons">menu</button>
-        </header>
-      `)
+// Hf7
 
-      data.cards.map(({title, sub, text}, i ) => document.getElementById('root').insertAdjacentHTML('beforeend', `
-        <div class="beercard">
-            <p class="number">${i+1}</p>
-            <p class="name">${title}</p>
-            <div class="basic-info">
-                <p>${sub}</p>
-                <p class="description">${text}</p>
-            </div>   
-            <button class="details-button">details <span class="material-icons arrow">arrow_forward</span></button>         
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    let authorId = Math.floor(Math.random() * users.length);
+    let author = users[authorId];
+
+    document.querySelector("article").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div id="author">
+            <p id="name">By ${author.name}</p>
+            <div id="author-info">
+                <p id="email"><a href="mailto:${author.email}">${author.email}</a></p>
+                <p id="phone">Tel: ${author.phone}</p>
+                <p id="company">Company:<br> ${author.company.name}</p>   
+            </div>              
         </div>
-        `))
-      
-    }).catch(function(err) {
-        console.log('Fetch problem show: ' + err.message);
-      }); */
+        `
+    );
+  } catch (error) {
+    console.log("Error fetching user data:", error);
+  }
 }
 
 window.addEventListener("load", loadEvent);
